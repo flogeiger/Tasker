1f7cbf17f6a9447f7c3d4cd96fd969452458c7f7
flo <florian.geiger@hexagonsi.com>
2023-11-28 20:56:12 +0100

    Created checkbox with trigger. Updates Progressbar in UI. Calles Constructure when checkbox is selected!

--- MVVM/ViewModels/MainViewModel.cs
+++ MVVM/ViewModels/MainViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PropertyChanged;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
@@ -8,7 +9,8 @@ using Tasker.MVVM.Models;
 
 namespace Tasker.MVVM.ViewModels
 {
-    public class MainViewModel
+	[AddINotifyPropertyChangedInterface]
+	public class MainViewModel
     {
 		public ObservableCollection<Category> Categories { get; set; }
 
