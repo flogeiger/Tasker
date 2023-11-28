23131982c30e137e352ed780542370930001ab0e
flo <florian.geiger@hexagonsi.com>
2023-11-28 18:49:11 +0100

    implemented color converter and implemented ProgressBar in GUI

--- MVVM/ViewModels/MainViewModel.cs
+++ MVVM/ViewModels/MainViewModel.cs
@@ -12,6 +12,8 @@ namespace Tasker.MVVM.ViewModels
     {
 		public ObservableCollection<Category> Categories { get; set; }
 
+		public ObservableCollection<MyTask> MyTasks { get; set; }
+
 		public MainViewModel()
 		{
 			FillData();
@@ -41,6 +43,84 @@ namespace Tasker.MVVM.ViewModels
 					}
 			   };
 
+			MyTasks = new ObservableCollection<MyTask>
+			   {
+					new MyTask
+					{
+						 TaskName = "Upload exercise files",
+						 Completed = false,
+						 CategoryId = 1
+					},
+					new MyTask
+					{
+						 TaskName = "Plan next course",
+						 Completed = false,
+						 CategoryId = 1
+					},
+					new MyTask
+					{
+						 TaskName = "Upload new ASP.NET video on YouTube",
+						 Completed = false,
+						 CategoryId = 2
+					},
+					new MyTask
+					{
+						 TaskName = "Fix Settings.cs class of the project",
+						 Completed = false,
+						 CategoryId = 2
+					},
+					new MyTask
+					{
+						 TaskName = "Update github repository",
+						 Completed = true,
+						 CategoryId = 2
+					},
+					new MyTask
+					{
+						 TaskName = "Buy eggs",
+						 Completed = false,
+						 CategoryId = 3
+					},
+					new MyTask
+					{
+						 TaskName = "Go for the pepperoni pizza",
+						 Completed = false,
+						 CategoryId = 3
+					},
+			   };
+
+			UpdateData();
+		}
+
+		public void UpdateData()
+		{
+			foreach (var c in Categories)
+			{
+				var tasks = from t in MyTasks
+							where t.CategoryId == c.Id
+							select t;
+
+				var completed = from t in MyTasks
+								where t.Completed == true
+								select t;
+
+				var notCompleted = from t in MyTasks
+								   where t.Completed == false
+								   select t;
+
+
+
+				c.PendingTasks = notCompleted.Count();
+				c.Percentage = (float)completed.Count() / (float)tasks.Count();
+			}
+			foreach (var t in MyTasks)
+			{
+				var catColor =
+					 (from c in Categories
+					  where c.Id == t.CategoryId
+					  select c.Color).FirstOrDefault();
+				t.TaskColor = catColor;
+			}
 		}
 	}
 }
--- obj/Debug/net7.0-windows10.0.19041.0/win10-x64/Tasker.AssemblyInfo.cs
+++ obj/Debug/net7.0-windows10.0.19041.0/win10-x64/Tasker.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tasker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+295d11ed98a89c6bca3096da8312d68f0aff8b35")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tasker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tasker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
