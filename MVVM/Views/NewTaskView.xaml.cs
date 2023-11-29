bd94191d0ca608729f5ac6559bbf2038d8b4b956
flo <florian.geiger@hexagonsi.com>
2023-11-29 13:58:39 +0100

    created final steps for Categories

--- MVVM/Views/NewTaskView.xaml.cs
+++ MVVM/Views/NewTaskView.xaml.cs
@@ -1,3 +1,4 @@
+using System.Text.RegularExpressions;
 using Tasker.MVVM.Models;
 
 namespace Tasker.MVVM.ViewModels;
@@ -37,6 +38,32 @@ public partial class NewTaskView : ContentPage
 				// Zeigt eine Fehlermeldung an, wenn keine Kategorie ausgewählt ist.
 				await DisplayAlert("Invalid Selection", "You must select a category!", "Ok");
 			}
+		}
+	}
+
+	private async void AddCategoryClicked(object sender, EventArgs e)
+	{
+		var vm = BindingContext as NewTaskViewModel;
+
+		string category = await DisplayPromptAsync(
+			"New Category", 
+			"Write the new category name", 
+			maxLength: 15, 
+			keyboard: Keyboard.Text);
+
+		var r = new Random();
+
+		if (!string.IsNullOrEmpty(category))
+		{
+			vm.Categories.Add(new Category
+			{
+				Id = vm.Categories.Max(x => x.Id) + 1,
+				Color = Color.FromRgb(
+					r.Next(0, 255),
+					r.Next(0, 255),
+					r.Next(0, 255)).ToHex(),
+				CategoryName = category
+			});
 		}
 	}
 }
--- obj/Debug/net7.0-android/Tasker.AssemblyInfo.cs
+++ obj/Debug/net7.0-android/Tasker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tasker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+a0039b60445a52aed317e4fe2b30f5a0c93c6027")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+06c665dbe45aa79fa8e73ac23c58dd1b86eade00")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tasker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tasker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
